2252b2b4cee7a86129fffd6d8ce52f4e37c273f3
Kirill Soloshenko <smertlove@yandex.ru>
2022-09-30 00:50:40 +0300

    in dev

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 
 import { entries } from "./entries.js";
 
-function make_entries(containerID, entries) { CardMaker._make_entries(containerID, entries); }
+function make_entries(containerID, entries) { EntryMaker._make_entries(containerID, entries); }
 
 function clear(containerID) { Clearer._clear(containerID); }
 
-export class CardMaker {
+export class EntryMaker {
 
     static make_entry(container, trans, hier) {
         let entry = document.createElement("div");
@@ -34,10 +34,8 @@ export class CardMaker {
                 hieroglyph.hidden = true;
                 hieroglyph.classList.add("fade-out");
                 hieroglyph.classList.remove("fade-in");
-
             }
         }
-
         let entryHr = document.createElement("hr");
         entryHr.classList.add("entry-hr");
         entry.append(entryHr);
@@ -60,33 +58,18 @@ export class CardMaker {
                 anchor.classList.add("tricky-anchor");
                 container.appendChild(anchor);
 
-
                 let lesson_hdr = document.createElement("p");
                 lesson_hdr.classList = ["lesson-hdr"];
                 lesson_hdr.innerText = "Урок " + cur_lesson;
 
-
-
-
-
                 container.appendChild(lesson_hdr);
-
-
 
                 let lessonLink = document.createElement("li");
                 lessonLink.innerHTML = '<a  href="#' + cur_lesson + '"> Урок ' + cur_lesson + '</a>';
                 document.getElementById("lesson-nav-menu").appendChild(lessonLink);
             }
             this.make_entry(container, element.translation, element.hieroglyph + " [" + element.pinyin + "]");
-
         });
-    }
-
-}
-
-class Clearer {
-    static _clear(containerID) {
-        document.getElementById(containerID).innerHTML = "";
     }
 }
 
