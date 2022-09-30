fce02d30af4df4f06cb7f5a76c29bd94c0d9ed64
Kirill Soloshenko <smertlove@yandex.ru>
2022-09-30 12:08:37 +0300

    #

--- js/cards.js
+++ js/cards.js
@@ -3,7 +3,8 @@ import { Cache } from "./Cache.js";
 import { entries } from "./entries.js";
 
 var card_rotated = false;
-
+var from = 15;
+var to = 20;
 
 var cache = new Cache(entries[getRandomInt(0, entries.length)], 10);
 
@@ -38,9 +39,17 @@ function setData(elem) {
 
     card.style.backgroundColor = colors[getRandomInt(0, colors.length)]
     setTimeout(function() { card_front.innerHTML = '<p>' + elem.translation + '</p>'; }, 150);
-    setTimeout(function() { card_back.innerHTML = '<p>' + elem.hieroglyph; + '</p>'; }, 350);
+    setTimeout(function() { card_back.innerHTML = '<p>' + elem.hieroglyph + '<br>' + elem.pinyin + '</p>'; }, 350);
+}
 
-
+function getRandomEntry(from, to) {
+    let temp = new Array();
+    entries.forEach(element => {
+        if (element.lesson >= from && element.lesson <= to) {
+            temp.push(element);
+        }
+    });
+    return temp[getRandomInt(0, temp.length)];
 }
 
 function _next() {
@@ -49,7 +58,7 @@ function _next() {
     }
 
     if (cache.isPtrInFront()) {
-        cache.append(entries[getRandomInt(0, entries.length)]);
+        cache.append(getRandomEntry(from, to));
     }
     cache.rmovPtr();
 
@@ -86,7 +95,44 @@ function rotate() {
 
 }
 
+// function __setFrom(i) {
+//     if (i > to) {
+//         alert("Пересмотрите взгляды на жизнь.")
+//     }
+//     from = i;
+// }
+
+// function __setTo(i) {
+//     if (i < from) {
+//         alert("Пересмотрите взгляды на жизнь.")
+//     }
+//     to = i;
+// }
+
+// function __fillDropdowns() {
+//     let st = document.getElementById("lessonStart");
+//     let fn = document.getElementById("lessonFinish");
+//     for (let i = 0; i < to; i++) {
+//         let link1 = document.createElement('a');
+//         let link2 = document.createElement('a');
+
+//         link1.text = i;
+//         link1.href = "#";
+//         link2.text = i;
+//         link2.href = "#";
+
+//         link1.className = "dropdown-item";
+//         link2.className = "dropdown-item";
+//         link1.onclick = function() { __setFrom(i) };
+//         link2.onclick = function() { __setTo(i) };
+//         st.appendChild(link1);
+//         fn.appendChild(link2);
+
+//     }
+// }
+
 setData(cache.getElem());
+// __fillDropdowns();
 document.getElementById("next").onclick = _next;
 document.getElementById("prev").onclick = _prev;
 document.getElementById("entry-card").onclick = rotate;
